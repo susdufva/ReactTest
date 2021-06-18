6ad647acb9d7ba41dd056ca4d7638523312aa0c6
dufva <sussiedufva@hotmail.com>
2021-06-18 10:35:16 +0200

    profile picture upload added

--- src/components/booking/ProfileSidebar.js
+++ src/components/booking/ProfileSidebar.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useHistory} from "react-router-dom";
+import axios from "axios"
 
 const username = localStorage.getItem("username")
 console.log(username)
@@ -7,12 +8,34 @@ console.log(username)
 function ProfileSidebar() {
 
     const history = useHistory()
+    const [fileData, setFileData] = useState()
+    const [id] = localStorage.getItem("userId")
 
     function Logout(){
       localStorage.clear()
       history.push("/")
     }
 
+    function handleImage(e){
+        setFileData(e.target.files[0])
+    }
+
+    function handleOnSubmit(e){
+        e.preventDefault()
+            const data = new FormData()
+            data.append("files", fileData)
+            data.append("ref", "user") //vilken collection tillhör bilden
+            data.append("refId", id) //vilken användare tillhör bilden
+            data.append("source", "users-permissions")
+            data.append("field", "image") //vilket fält i collection
+            axios.post('http://localhost:1337/upload', data)
+            .then(res => {
+                console.log("res", res)
+                
+            })
+        
+    }
+    
     return (
         <>
           
@@ -50,8 +73,14 @@ function ProfileSidebar() {
             <li>
                 <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                 <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-user"></i></span>
-                <span className="text-sm font-medium">Profile</span>
+                <span className="text-sm font-medium">Profilbild</span>
                 </a>
+            </li>
+            <li>
+               <form onSubmit={handleOnSubmit}>
+                   <input type="file" name="file" id="" onChange={handleImage}></input>
+                   <button type="submit">Ladda upp</button>
+               </form>
             </li>
             <li>
                 <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
@@ -61,9 +90,19 @@ function ProfileSidebar() {
                 </a>
             </li>
             <li>
+            <a className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800" href="#">
+                    <svg class="h-5 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.3246 4.31731C10.751 2.5609 13.249 2.5609 13.6754 4.31731C13.9508 5.45193 15.2507 5.99038 16.2478 5.38285C17.7913 4.44239 19.5576 6.2087 18.6172 7.75218C18.0096 8.74925 18.5481 10.0492 19.6827 10.3246C21.4391 10.751 21.4391 13.249 19.6827 13.6754C18.5481 13.9508 18.0096 15.2507 18.6172 16.2478C19.5576 17.7913 17.7913 19.5576 16.2478 18.6172C15.2507 18.0096 13.9508 18.5481 13.6754 19.6827C13.249 21.4391 10.751 21.4391 10.3246 19.6827C10.0492 18.5481 8.74926 18.0096 7.75219 18.6172C6.2087 19.5576 4.44239 17.7913 5.38285 16.2478C5.99038 15.2507 5.45193 13.9508 4.31731 13.6754C2.5609 13.249 2.5609 10.751 4.31731 10.3246C5.45193 10.0492 5.99037 8.74926 5.38285 7.75218C4.44239 6.2087 6.2087 4.44239 7.75219 5.38285C8.74926 5.99037 10.0492 5.45193 10.3246 4.31731Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
+                    <span className="text-sm font-medium">Settings</span>
+                </a>
+            </li>
+            <li>
                 <a onClick={Logout} className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-log-out"></i></span>
-                <span className="text-sm font-medium">Logout</span>
+                <button className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-log-out"></i></button>
+                <button className="text-sm font-medium">Logga ut</button>
                 </a>
             </li>
             </ul>
